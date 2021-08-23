c0932eb9c6b84bf8e115647d874f4148d7178636
andrmiw9 <andreymiw@yahoo.com>
2021-08-23 21:09:48 +0300

    draw line to mouse pos

--- Scenes and scripts/Cannon.gd
+++ Scenes and scripts/Cannon.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var rock = preload("res://Scenes and scripts/Rock.tscn").instance()
 var spawn_offset := Vector2(100,100)
-var rock_speed = 100
+var rock_speed = 1000
 
 
 func _input(event):
@@ -13,7 +13,7 @@ func _input(event):
 #		print(impulse)
 #		rock.position = position + impulse
 		rock.position = $Dulo/SpawnLoc.get_global_position()
-		rock.rotation = $Dulo.rotation
+		rock.rotation = $Dulo.get_global_rotation()
 		rock.apply_impulse(Vector2(), Vector2(rock_speed,0).rotated($Dulo.rotation))
 		get_viewport().get_node("Testworld").add_child(rock)
 		#rock.launch(-impulse)
@@ -22,3 +22,17 @@ func _input(event):
 
 func _physics_process(delta):
 	$Dulo.look_at(get_global_mouse_position())
+
+
+func _ready():
+	_draw()
+
+
+func _draw():
+	draw_line(Vector2(0,0), get_global_mouse_position() - global_position, Color.red, 5)
+	print("here1")
+
+
+# warning-ignore:unused_argument
+func _process(delta:float)->void:	# для обновления встроенной функции _draw
+	update()
